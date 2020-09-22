b6855149d8fb47bae8f12ae49712aa37b92800d6
Jordan <climbjn189@gmail.com>
2020-09-21 22:19:07 -0400

    Client side code mostly done

--- public/js/script.js
+++ public/js/script.js
@@ -1,16 +1,61 @@
 "use strict";
+//TODO: VALIDATE FUNCTION FOR INITIAL USER INPUT AND FOR CHANGING DATA
 
 //Get all items for a user
-function getAllItems(uid) {
-    ;
+function getAllItems(username) {
+    fetch('/data', {
+        method: "POST",
+        body: JSON.stringify({username: username}),
+        headers: {"Content-Type":"application/json"}
+    })
+    .then(response => response.json())
+    .then(function(data) {
+        console.log(data);
+        /* TODO: UNCOMMENT THIS WHEN READY
+        for (let i = 0; i < data.length; i++) {
+            addTableEntry(data[i]);
+        }
+        */
+    })
 }
 
-function editField() {
-    ;
+//Edit a field using a prompt
+function editField(e) {
+    console.log("Edit");
+    //console.log(e.target.parentElement);
+    let textNode = e.target.parentElement.firstChild;
+    let entryID = e.target.parentElement.parentElement.id;
+    let newValue = window.prompt("Enter a new value", textNode.data);
+    
+    let data = {user: username, id: entryID};
+    data[e.target.parentElement.className] = newValue;
+    fetch('/edit', {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {"Content-Type":"application/json"}
+    })
+    .then(function(response) {
+        if (response.ok) {
+            textNode.data = newValue;
+        }
+    })
 }
 
-function deleteItem() {
-    ;
+//Delete an item from the database and list
+function deleteItem(e) {
+    console.log("Delete");
+    //console.log(e);
+    let row = e.target.parentElement.parentElement;
+    fetch('/delete', {
+        method: "POST",
+        body: JSON.stringify({username: username, id: row.id}),
+        headers: {"Content-Type":"application/json"}
+    })
+    .then(function(response) {
+        if (response.ok) {
+            row.remove();
+        }
+    })
 }
 
 //Add one json object to the table as the last row
@@ -19,21 +64,35 @@ function addTableEntry(jsonData) {
     const tableBody = document.getElementById("mealBody");
     //Insert a new row and then new cells
     let row = tableBody.insertRow(-1);
+    row.id = jsonData.id;
 
     let [hours, minutes] = jsonData.time.split(":");
-    time = ((hours > 12)? hours-12 : hours) + ":" + minutes + " " + ((hours >= 12)? 'PM' : 'AM');
+    time = ((hours > 12)? hours-12 : hours-0) + ":" + minutes + " " + ((hours >= 12)? 'PM' : 'AM');
 
-   row.insertCell(0).innerHTML = time + "<button class=\"edit\">?</button>";
-   row.insertCell(1).innerHTML = jsonData.food + "<button class=\"edit\">?</button>";
-   row.insertCell(2).innerHTML = jsonData.calories + "<button class=\"edit\">?</button>";
-   row.insertCell(3).innerHTML = jsonData.meal + "<button class=\"edit\">?</button>";
-   row.insertCell(4).innerHTML = "<button class=\"delete\">Delete?</button>";
+   let cell0 = row.insertCell(0);
+   cell0.className = "time";
+   cell0.innerHTML = time;// + "<button class=\"edit\">✎</button>";
+
+   let cell1 = row.insertCell(1);
+   cell1.className = "food";
+   cell1.innerHTML = jsonData.food + "<button class=\"edit\">✎</button>";
+
+   let cell2 = row.insertCell(2);
+   cell2.className = "calories";
+   cell2.innerHTML = jsonData.calories + "<button class=\"edit\">✎</button>";
+
+   let cell3 = row.insertCell(3);
+   cell3.className = "meal";
+   cell3.innerHTML = jsonData.meal + "<button class=\"edit\">✎</button>";
+
+   row.insertCell(4).innerHTML = "<button class=\"delete\">✘</button>";
 }
 
 //Submits form data to the server
 function submitFormData(e) {
     e.preventDefault();
     let data = {};
+    data["username"] = username;
     
     let fields = document.getElementsByClassName("entry");
     for (let i = 0; i < fields.length; i++) {
@@ -60,10 +119,23 @@ function submitFormData(e) {
     });
 }
 
-window.onload = function() {
-    //getAllItems(uid);
-    document.getElementById("submit").addEventListener("click", submitFormData, false);
-    //document.addEventListener("click", handleButton, false);
+//Handles edit button clicks and delete button clicks
+function clickHandler(e) {
+    if (e.target.tagName === "BUTTON") {
+        if (e.target.className === "edit") {
+            editField(e);
+        } else if (e.target.className === "delete") {
+            deleteItem(e);
+        }
+    }
 }
 
+window.onload = function() {
+    username = "bob";
+    getAllItems(username);
+    document.getElementById("submit").addEventListener("click", submitFormData, false);
+    document.addEventListener("click", clickHandler, false);
+}
+
+let username = null;
 console.log("Loaded JS")
--- server.js
+++ server.js
@@ -9,9 +9,34 @@ const bodyParser = require('body-parser')
 app.use(express.static('public'))
 
 app.post("/submit", bodyParser.json(), function(request, response) {
-    //write post request code here
+    //write post request code for a new item here
+    let data = request.body;
+    //CHANGE THIS WITH THE DATABASE ID
+    data["id"] = Math.floor((Math.random() * 1000) + 1);
+    console.log("Submit");
+    console.log(data);
+    response.json(data);
+})
+
+app.post("/edit", bodyParser.json(), function(request, response) {
+    //write post request code for an edited item here
+    console.log("Edit");
     console.log(request.body);
-    response.json(request.body);
+    response.sendStatus(200);
+})
+
+app.post("/delete", bodyParser.json(), function(request, response) {
+    //write post request code for a deleted item here
+    console.log("Delete");
+    console.log(request.body);
+    response.sendStatus(200);
+})
+
+app.post("/data", bodyParser.json(), function(request, response) {
+    //write post request code for getting all data for a user
+    console.log("Get Data");
+    console.log(request.body);
+    response.json({data: "true"});
 })
 
 app.listen(3000)
