525f9fb728981436aaa59daf41113adfa27b0b3d
Tom Graham <61550224+tgrahamcodes@users.noreply.github.com>
2020-09-24 17:20:38 -0400

    adding data+ deleting data is working with mongodb

--- public/script.js
+++ public/script.js
@@ -1,36 +1,51 @@
 const tasksList = document.getElementById("tasks");
 const tasksForm = document.querySelector("form");
 
-
 // a helper function that creates a list item for a given tasks
-function appendNewtasks(tasks) {
+function appendNewTask(tasks, id) {
   const newListItem = document.createElement("li");
-  newListItem.innerText = tasks;
+  newListItem.innerText = tasks.name;
+
+  newListItem.onclick = function(){
+    fetch( '/delete', {
+      method:'POST',
+      body: JSON.stringify({ _id:tasks._id }), 
+      headers:{
+        'Content-Type':'application/json'
+      }
+    })
+    .then( response => response.json() )
+    .then( json => {
+      newListItem.remove()
+     })
+  }
   tasksList.appendChild(newListItem);
 }
 
 // fetch the initial list of tasks
 fetch("/tasks")
-  .then(response => response.json()) 
+  .then(response => response.json())
   .then(tasks => {
-    tasksList.firstElementChild.remove();
-    tasks.forEach(appendNewtasks);
+    tasksList.firstElementChild.remove() // removes loading
+    tasks.forEach(appendNewTask);
 });
 
 // listen for the form to be submitted and add a new tasks when it is
 tasksForm.addEventListener("submit", event => {
   event.preventDefault();
-  
+
+  let newTask = tasksForm.elements.tasks.value;
+
   fetch('/add', {
     method:'POST',
-    body: JSON.stringify({ tasks:tasksForm.elements.tasks.value }),
+    body: JSON.stringify({ name:newTask }),
     headers:{
-      "Content-Type":"application/json"
+      'Content-Type':'application/json'
     }
   })
   .then( response => response.json() )
   .then( json => {
-    appendNewtasks( json.tasks );
+    appendNewTask( json.tasks, json._id  );
    })
 
   tasksForm.reset();
--- server.js
+++ server.js
@@ -1,32 +1,54 @@
 const express = require('express'),
       bodyParser = require('body-parser'),
+      mongodb = require('mongodb'),
+      MongoClient = mongodb.MongoClient,
+      dotenv = require('dotenv').config(),
       app = express();
 
-const tasks = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
+const uri = `mongodb+srv://admin:${process.env.DB_PASSWORD}@cluster0.lkq2d.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {  useNewUrlParser: true, useUnifiedTopology: true  });
 
-// make all the files in 'public' available
+let collection = null
+client.connect(err => {
+  collection = client.db(`${process.env.DB_NAME}`).collection("tasks");
+});
+
 app.use(express.static("public"));
 
 app.get("/", (request, response) => {
   response.sendFile(__dirname + "/views/index.html");
 });
 
-// send the default array of tasks to the webpage
-app.get("/tasks", (request, response) => {
-  // express helps us take JS objects and send them as JSON
-  response.json(tasks);
-});
-
-// bodyParser will be called in between the request and the response
-app.post("/add", bodyParser.json(), (request, response) => {
-  tasks.push( request.body.tasks )
-  response.json( request.body )
+/* Whenever a client requests the /tasks route we send the 
+* JSON object back down to the client.
+*/
+app.get('/tasks', bodyParser.json(), function(request, response){
+  collection.find({}).toArray(function(err, result){
+    if (err) throw err;
+    console.log(result);
+    response.json(result);
+  })
 })
 
+/* Handles a POST request from the client and then bodyParser will be 
+* called in between the request and the response will push the data to
+* the database on mongodb and return the response
+*/
+app.post('/add', bodyParser.json(), function(request, response) {
+  console.log( 'add body:', request.body )
+  collection.insertOne( request.body ).then( dbresponse => {
+    response.json( dbresponse.ops[0] )
+  })
+})
+
+app.post('/delete', bodyParser.json(), function(request, response){
+  console.log('delete body:', request.body)
+  collection.deleteOne({ _id:mongodb.ObjectID( request.body._id ) })
+  .then( result => response.json( result ) )
+})
+
+/* Setting the port manually so that it doesn't randomize on my local machine 
+*/
 process.env.PORT = 3000;
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
