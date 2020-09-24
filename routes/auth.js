ad9d61ef6b5bf4f751675d723cbcd7ac454375a5
Jacob Tutlis <jtutlis@gmail.com>
2020-09-24 17:09:22 -0400

    testing cb

---
+++ routes/auth.js
@@ -0,0 +1,23 @@
+const router = require("express").Router();
+const passport = require("passport");
+
+// login github
+router.get(
+    "/github",
+    passport.authenticate("github", {
+        scope: ["profile"],
+    })
+);
+
+// logout
+router.get("/logout", (req, res) => {
+    res.send("logout");
+});
+
+//callback github
+router.get("/github/redirect", passport.authenticate("github"), (req, res) => {
+    res.send(req.user);
+    // res.redirect("/profile");
+});
+
+module.exports = router;
