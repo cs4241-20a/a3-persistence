e72e84ca3981df84076f8480dab9d18dcd3a3c7d
Jacob Tutlis <jtutlis@gmail.com>
2020-09-25 15:53:34 -0400

    added userID to model

--- models/movie-model.js
+++ models/movie-model.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const movieSchema = new Schema({
     movieName: String,
     seen: String,
+    userID: String,
 });
 
 const Movie = mongoose.model("movie", movieSchema);
