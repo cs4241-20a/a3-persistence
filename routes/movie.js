3d35d490f6297ce426f95dd8a8a1c5e401f14746
Jacob Tutlis <jtutlis@gmail.com>
2020-09-25 15:49:32 -0400

    testing user2

--- routes/movie.js
+++ routes/movie.js
@@ -5,37 +5,41 @@ const passport = require("passport");
 //callback github
 router.post("/movie/add", (req, res) => {
     console.log(req.body);
-    Movie.findOne({ movieName: req.body.movieName, userID: req.user }).then(
-        (currentMovie) => {
-            if (currentMovie) {
-                console.log("current movie:", currentMovie);
-                if (currentMovie.seen != req.body.seen) {
-                    Movie.updateOne(
-                        { movieName: req.body.movieName, userID: req.user },
-                        { seen: req.body.seen }
-                    ).then((updatedMovie) => {
-                        console.log("updated movie:", updatedMovie);
-                    });
-                }
-
-                res.end("ok");
-            } else {
-                new Movie({
-                    movieName: req.body.movieName,
-                    seen: req.body.seen,
-                    userID: req.user,
-                })
-                    .save()
-                    .then((newMovie) => {
-                        console.log("new movie:", newMovie);
-                        res.end("ok");
-                    });
+    Movie.findOne({
+        movieName: req.body.movieName,
+        userID: req.user.githubID,
+    }).then((currentMovie) => {
+        if (currentMovie) {
+            console.log("current movie:", currentMovie);
+            if (currentMovie.seen != req.body.seen) {
+                Movie.updateOne(
+                    {
+                        movieName: req.body.movieName,
+                        userID: req.user.githubID,
+                    },
+                    { seen: req.body.seen }
+                ).then((updatedMovie) => {
+                    console.log("updated movie:", updatedMovie);
+                });
             }
+
+            res.end("ok");
+        } else {
+            new Movie({
+                movieName: req.body.movieName,
+                seen: req.body.seen,
+                userID: req.user,
+            })
+                .save()
+                .then((newMovie) => {
+                    console.log("new movie:", newMovie);
+                    res.end("ok");
+                });
         }
-    );
+    });
 });
 
-router.post("/movie", (req, res) => {
+router.get("/movie", (req, res) => {
     res.send(req.user);
 });
 
