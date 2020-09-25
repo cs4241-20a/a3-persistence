cf82863d442270e55a4493946ef7e5472f9a85bb
unknown <jstoessel@wpi.edu>
2020-09-25 19:29:40 -0400

    updated server.js to express instead of HTML

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const { response } = require('express');
 
 // our default array of dreams
 let scoreboard = [
@@ -11,16 +12,39 @@ let scoreboard = [
 // checks for objects in the public folder
 app.use(express.static('public'));
 
-// https://expressjs.com/en/starter/basic-routing.html
+// default
 app.get("/", (request, response) => {
   response.sendFile(__dirname + "/views/index.html");
 });
 
-// send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
+// get data for building initial table
+app.get("/data", (request, response) => {
   // express helps us take JS objects and send them as JSON
-  response.json(dreams);
+  response.json(scoreboard);
 });
+
+// adds a new user to the scoreboard array from post method
+app.post("/submit", bodyParser.json(), (request,response) => {
+  scoreboard.push(request.body);
+  console.log("New user recoded!");
+  response.json(scoreboard);
+})
+
+// deletes a user from the scoreboard array and returns new scoreboard
+app.post("/delete", bodyParser.json(), (request, response) => {
+  let delName = request.body.name;
+  scoreboard = scoreboard.filter(data => data.name !== delName);
+  response.json(scoreboard);
+})
+
+// modifies a given user's score and generates a new CPS, returns new scoreboard.
+app.post("/modify", bodyParser.json(), (request, reponse) => {
+
+  let cps = Math.round(((request.body.clicks) / request.body.seconds) * 10) / 10;
+  // set user's cps to given cps
+
+  response.json(scoreboard);
+})
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
