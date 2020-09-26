a98a006aed574c33825599b896677f6d3f0ea060
unknown <jstoessel@wpi.edu>
2020-09-25 20:09:53 -0400

    Fixed html links, express adds new user as undefined.

--- public/script.js
+++ public/script.js
@@ -1,41 +1,211 @@
-// client-side js, loaded by index.html
-// run by the browser each time the page is loaded
+let clickcount = 0;
+let seconds = 0;
+let cps = 0;
 
-console.log("hello world :o");
-
-// define variables that reference elements on our page
-const dreamsList = document.getElementById("dreams");
-const dreamsForm = document.querySelector("form");
-
-// a helper function that creates a list item for a given dream
-function appendNewDream(dream) {
-  const newListItem = document.createElement("li");
-  newListItem.innerText = dream;
-  dreamsList.appendChild(newListItem);
+//when start button is clicked swap visibilities and start 30 second timer.
+function startClicked() {
+  seconds = document.getElementById('customseconds').value;
+  if (seconds > 0) {
+    console.log("Game started!");
+    document.getElementById('customseconds').style.display = "none";
+    document.getElementById('startbtn').style.display = "none";
+    document.getElementById('currentclicks').innerHTML = "Click the button to start earning points!";
+    var classes = document.getElementsByClassName('poststart');
+    for (var i = 0; i < classes.length; i++) {
+      classes[i].style.display = "block";
+    }
+    setTimeout(end, seconds * 1000); //after 3 seconds...
+  } else {
+    alert("Please give seconds greater than 0");
+  }
 }
 
-// fetch the initial list of dreams
-fetch("/dreams")
-  .then(response => response.json()) // parse the JSON from the server
-  .then(dreams => {
-    // remove the loading text
-    dreamsList.firstElementChild.remove();
-  
-    // iterate through every dream and add it to our page
-    dreams.forEach(appendNewDream);
-  
-    // listen for the form to be submitted and add a new dream when it is
-    dreamsForm.addEventListener("submit", event => {
-      // stop our form submission from refreshing the page
-      event.preventDefault();
 
-      // get dream value and add it to the list
-      let newDream = dreamsForm.elements.dream.value;
-      dreams.push(newDream);
-      appendNewDream(newDream);
+//increments number of clicks by 1 per click.
+function addClicked() {
+  clickcount += 1;
+  document.getElementById('currentclicks').innerHTML = clickcount + " Points";
+}
 
-      // reset form
-      dreamsForm.reset();
-      dreamsForm.elements.dream.focus();
-    });
-  });
+//after timer is elapsed disable button and allow recording.
+function end() {
+  console.log("time is up");
+  document.getElementById('clickbtn').style.display = "none"; //make click button disappear
+  alert("You've run out of time!"); //give user alert to know of change
+
+  if (clickcount === 0) {
+    document.getElementById('currentclicks').innerHTML = "Wow... you didn't even try";
+    document.getElementById('startbtn').style.display = "block";
+  } else {
+    document.getElementById('currentclicks').innerHTML = "You scored: " + clickcount + " Points!";
+    cps = Math.round((clickcount / seconds) * 10) / 10;
+    console.log(clickcount);
+    console.log(seconds);
+    console.log(cps);
+
+    //make game recording buttons appear
+    let classes = document.getElementsByClassName('postgame'); 
+    for (var i = 0; i < classes.length; i++) {
+      classes[i].style.display = "block";
+    }
+  }
+}
+
+const submit = function (e) {
+  //prevent default form action from being carried out
+  e.preventDefault();
+
+  if (document.getElementById('yourname').value === "") {
+    alert("Please don't leave the name blank");
+    return false;
+  }
+
+  const userScore = {
+    name: document.getElementById('yourname').value,
+    clicks: clickcount,
+    seconds: seconds
+  }
+
+  const body = JSON.stringify(userScore);
+
+  fetch('/submit', {
+    method: 'POST',
+    body
+  })
+    .then(function (response) {
+      //response
+      response.json().then(function (data) {
+        //data
+        console.log("Submit Response:", response);
+        console.log("Returned data: ", data);
+        restartGame();
+
+        buildTable(data);
+      })
+    })
+
+  return false;
+}
+
+const deleteName = function (e) {
+  //prevent default form action from being carried out
+  e.preventDefault();
+
+  const delName = {
+    name: document.getElementById('delname').value
+  }
+  // if (delName.name == "Mr. Insano") {
+  //   alert("Well well well, looks like someone couldn't beat Mr. Insano like a true pro... tsk tsk");
+  // }
+
+  const body = JSON.stringify(delName);
+
+  fetch('/delete', {
+    method: 'POST',
+    body
+  })
+    .then(function (response) {
+      //response
+      response.json().then(function (data) {
+        //data
+        console.log("Delete Response:", response);
+        console.log("Returned data: ", data);
+
+        buildTable(data);
+        document.getElementById('delname').value = "";
+      })
+    })
+
+  return false;
+}
+
+//what to do after name is stored in server
+function restartGame() {
+  console.log("Restarting game...");
+  clickcount = 0;
+
+
+  document.getElementById('yourname').value = "";
+  document.getElementById('currentclicks').style.display = "none";
+  document.getElementById('inputname').style.display = "none";
+  document.getElementById('yourname').style.display = "none";
+  document.getElementById('submitbtn').style.display = "none";
+  document.getElementById('startbtn').style.display = "block";
+  document.getElementById('customseconds').style.display = "block";
+  document.getElementById('customseconds').value = "Please input seconds here";
+}
+
+//generate a table for displaying under score
+function buildTable(newScoreboard) {
+
+  //sort by clicks per second
+  newScoreboard.sort((a, b) => b.cps - a.cps);
+
+  let table = document.getElementById('scoretable');
+  //for building the scoreboard header for the table
+  let newTable =
+    '<tr>\n' +
+    '<th>Placing</th>\n' +
+    '<th>Player Name</th>\n' +
+    '<th>Clicks Per Second</th>\n' +
+    '<th>Total Clicks</th>\n' +
+    '<th>Seconds</th>\n' +
+    '<th>Input Time (H:min)</th>\n' +
+    '</tr>\n';
+
+  //for populating the scoreboard with scores.
+  for (let i = 0; i < newScoreboard.length; i++) {
+    let d = new Date(newScoreboard[i].time);
+    let s = d.getMinutes();
+    // if ((d.getMinutes() % 10) == 0) {
+    //   s = d.getMinutes().toString() + "0";
+    // }
+    if (d.getMinutes() < 10) {
+      s = "0" + d.getMinutes().toString();
+    }
+    newTable += ('<tr>\n');
+    newTable += (`<td> #${i + 1}</td>\n`);
+    newTable += (`<td> ${newScoreboard[i].name}</td>\n`);
+    newTable += (`<td> ${newScoreboard[i].cps}</td>\n`);
+    newTable += (`<td> ${newScoreboard[i].clicks}</td>\n`);
+    newTable += (`<td> ${newScoreboard[i].seconds}</td>\n`);
+    newTable += (`<td> ${d.getHours()}:${s}</td>\n`);
+    newTable += ('</tr>\n');
+  }
+  table.innerHTML = newTable;
+  //console.log("Table populated: \n" + table.innerHTML);
+}
+
+//get the table data for generating the table on load.
+function initializeTable(){
+  fetch('/data', {
+    method: 'GET'
+  })
+    .then(function (response) {
+      //response
+      response.json().then(function (data) {
+        //data
+        console.log("Data Response:", response);
+        console.log("Returned data: ", data);
+
+        buildTable(data);
+      })
+    })
+
+  return false;
+}
+
+window.onload = function () {
+  const button = document.getElementById('submitbtn');
+  button.onclick = submit;
+  const delbutton = document.getElementById('delbtn');
+  delbutton.onclick = deleteName;
+  document.getElementById('customseconds').value = "Please input seconds here";
+  document.getElementById('clickbtn').style.display = "none";
+  document.getElementById('currentclicks').style.display = "none";
+  document.getElementById('inputname').style.display = "none";
+  document.getElementById('yourname').style.display = "none";
+  document.getElementById('submitbtn').style.display = "none";
+  initializeTable();
+  console.log("Loaded!");
+}
