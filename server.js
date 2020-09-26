f96c2647cf95675b6b6dd470a7de7cf8f43034e4
athammana <aj@virpietech.com>
2020-09-26 17:49:46 -0400

    got port from process env

--- server.js
+++ server.js
@@ -1,16 +1,10 @@
 var express = require('express');
 var app = express();
-
+port = 3000;
 
 app.use(express.static("public"));
-// app.get('/', function (req, res) {
-// 	res.sendFile(__dirname + '/public/index.html');
-// });
 
-// app.get('/login', function (req, res) {
-// 	res.sendFile(__dirname + '/public/login.html');
-// });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(process.env.PORT || port, function () {
+  console.log('Example app listening on port', process.env.PORT || port);
 });
