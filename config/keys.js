6f62a12e4f928a8306a34723d6f82e67ef97081e
Glitch (trail-lofty-player) <none>
2020-09-26 21:41:30 +0000

    Login implemented, needs adjustments and styling

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+const dbPassword = 'mongodb+srv://UserA3:webware123@a3-cluster.gsvjm.azure.mongodb.net/USERS?retryWrites=true&w=majority';
+
+module.exports = {
+    mongoURI: dbPassword
+};
--- server.js
+++ server.js
@@ -4,19 +4,76 @@
 // we've started you off with Express (https://expressjs.com/)
 // but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require("express");
+const expressLayouts = require('express-ejs-layouts');
 const bodyParser = require("body-parser");
 const app = express();
+const mongoose = require('mongoose');
+const passport = require('passport');
+const flash = require('connect-flash');
+const session = require('express-session');
+
+//passport config
+require('./config/passport')(passport)
+
+//DB config
+const db = require('./config/keys.js').mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true ,useUnifiedTopology: true}
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+//EJS
+app.use(expressLayouts)
+app.set('view engine', 'ejs')
+
+//bodyparser
+app.use(express.urlencoded({ extended: true}))
+
+//express session
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true
+}))
+
+//passport
+app.use(passport.initialize())
+app.use(passport.session())
+
+//connect flash
+app.use(flash())
+
+//global vars
+app.use((req,res,next) => {
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.error_msg = req.flash('error_msg')
+  res.locals.error = req.flash('error')
+  next();
+});
 
 // make all the files in 'public' available
 // https://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
 
+//routes
+app.use('/', require('./routes/index'))
+app.use('/users', require('./routes/users'))
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
+
 // https://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
+/* app.get("/", (request, response) => {
   response.sendFile(__dirname + "/login.html");
-});
+});*/
 
-
+/*
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
@@ -48,4 +105,4 @@ app.post("/delete", bodyParser.json(), function(request, response){
     collection
     .deleteOne({ _id:mongodb.ObjectID( request.body.id ) })
     .then( result => response.json( result ) )
-})
+})*/
