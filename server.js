d0016f5076eb233e79bce85729682a77f1995789
unknown <jstoessel@wpi.edu>
2020-09-27 15:10:36 -0400

    created env file and updated server for mongodb

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-
+require('dotenv').config()
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const { response } = require('express');
-require('dotenv').config()
 
-// our default array of dreams
-let scoreboard = [
-  { name: "Mr. Insano", cps: 105.1, clicks: 1051, seconds: 10, time: 7987989869 },
-  { name: "Matthew", cps: 5.4, clicks: 54, seconds: 10, time: 7987097986986 }
-];
+// // our default array of scoreboards
+// let scoreboard = [
+//   { name: "Mr. Insano", cps: 105.1, clicks: 1051, seconds: 10, time: 7987989869 },
+//   { name: "Matthew", cps: 5.4, clicks: 54, seconds: 10, time: 7987097986986 }
+// ];
 
 // checks for objects in the public folder
 app.use(express.static('public'));
@@ -25,29 +24,7 @@ app.get("/data", (request, response) => {
   response.json(scoreboard);
 });
 
-// submits a new user to the scoreboard array from post method
-app.post("/submit", bodyParser.json(), (request,response) => {
-  console.log("The person is: " + request.body.name);
-  scoreboard.push(request.body);
-  console.log("New user recorded!");
-  response.json(scoreboard);
-})
 
-// deletes a user from the scoreboard array and returns new scoreboard
-app.post("/delete", bodyParser.json(), (request, response) => {
-  console.log("Deleting...");
-  scoreboard = scoreboard.filter(data => data.name !==  request.body.name);
-  response.json(scoreboard);
-})
-
-// modifies a given user's score and generates a new CPS, returns new scoreboard.
-app.post("/modify", bodyParser.json(), (request, reponse) => {
-
-  let cps = Math.round(((request.body.clicks) / request.body.seconds) * 10) / 10;
-  // set user's cps to given cps
-
-  response.json(scoreboard);
-})
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
@@ -58,10 +35,45 @@ const listener = app.listen(process.env.PORT, () => {
 
 const mongodb = require('mongodb')
 const MongoClient = mongodb.MongoClient;
-const uri = 'mongodb+srv://Jordan:lol@cluster0.i1bsg.mongodb.net/<dbname>?retryWrites=true&w=majority';
+const uri = 'mongodb+srv://Jordan:${process.env.DB_PASS}@cluster0.i1bsg.mongodb.net/<dbname>?retryWrites=true&w=majority';
 const client = new MongoClient(uri, { useNewUrlParser: true });
+
+let collection = null;
 client.connect(err => {
-  const collection = client.db("test").collection("devices");
+  collection = client.db("test").collection("devices");
   // perform actions on the collection object
-  client.close();
+
+  // client.close();
 });
+
+
+
+// submits a new user to the scoreboard array from post method
+app.post("/submit", bodyParser.json(), (request,response) => {
+  console.log("The person is: " + request.body.name);
+  collection.insertOne(request.body)
+    .then( debresponse => {
+      console.log( dbresponse);
+    })
+  // scoreboard.push(request.body);
+  console.log("New user recorded!");
+
+  response.json(dbresponse.ops[0]);
+  // response.json(scoreboard);
+})
+
+// deletes a user from the scoreboard array and returns new scoreboard
+app.post("/delete", bodyParser.json(), (request, response) => {
+  console.log("Deleting...");
+  scoreboard = scoreboard.filter(data => data.name !==  request.body.name);
+  response.json(scoreboard);
+})
+
+// modifies a given user's score and generates a new CPS, returns new scoreboard.
+// app.post("/modify", bodyParser.json(), (request, reponse) => {
+
+//   let cps = Math.round(((request.body.clicks) / request.body.seconds) * 10) / 10;
+//   // set user's cps to given cps
+
+//   response.json(scoreboard);
+// })
