843b608b3d4a0176e2e7283859f177060b929414
MargaretEarnest <molly7312000@gmail.com>
2020-09-27 01:12:01 -0400

    Connected the app to a database, added a profile page, and added maze annotations

--- public/script.js
+++ public/script.js
@@ -1,53 +1,28 @@
-// // define variables that reference elements on our page
-// const dreamsList = document.getElementById("dreams");
-// const dreamsForm = document.querySelector("form");
+var mouseDown = false;
 
-// // a helper function that creates a list item for a given dream
-// function appendNewDream(dream) {
-//   const newListItem = document.createElement("li");
-//   newListItem.innerText = dream;
-//   dreamsList.appendChild(newListItem);
-// }
+document.body.onmousedown = function() {
+  mouseDown = true;
+};
+document.body.onmouseup = function() {
+  mouseDown = false;
+};
 
-// // fetch the initial list of dreams
-// fetch("/dreams")
-//   .then(response => response.json()) // parse the JSON from the server
-//   .then(dreams => {
-//     // remove the loading text
-//     dreamsList.firstElementChild.remove();
-  
-//     // iterate through every dream and add it to our page
-//     dreams.forEach(appendNewDream);
-//   });
-
-// // listen for the form to be submitted and add a new dream when it is
-// dreamsForm.addEventListener("submit", event => {
-//   // stop our form submission from refreshing the page
-//   event.preventDefault();
-
-//   fetch("/add", {
-//     method: "POST",
-//     body: JSON.stringify({dream: dreamsForm.elements.dream.value}),
-//     headers: {
-//       "Content-Type":"application/json"
-//     }
-//   })
-//   .then(res => res.json())
-//   .then(json => {
-//     appendNewDream(json.dream);
-//   })
-
-//   // reset form
-//   dreamsForm.reset();
-//   dreamsForm.elements.dream.focus();
-// });
+function makePath(cell) {
+  if(mouseDown) {
+    if(cell.style.backgroundColor == "rgb(227, 227, 227)") {
+      cell.style.cssText += "background-color: #ADD8E6";
+    } else {
+      cell.style.cssText += "background-color: #E3E3E3";
+    }
+  }
+}
 
 //maze generation
 class Cell {
   constructor(wallList) {
-    this.walls = wallList
+    this.walls = wallList;
   }
-  
+
   getWalls() {
     return this.walls;
   }
@@ -55,111 +30,234 @@ class Cell {
 
 class Wall {
   constructor() {
-    this.exists = true
-    this.owner = []
+    this.exists = true;
+    this.owner = [];
   }
-  
+
   breakWall() {
-    this.exists = false
+    this.exists = false;
   }
-  
+
   addOwner(newOwner) {
-    this.owner.push(newOwner)
+    this.owner.push(newOwner);
   }
 }
 
 //from here https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
 function randInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min)
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 function makeMaze(mazeCol, mazeRow) {
-  let grid = []
-  for(let i=0; i<mazeRow; i++) {
-    grid.push(new Array(mazeCol))
+  let grid = [];
+  for (let i = 0; i < mazeRow; i++) {
+    grid.push(new Array(mazeCol));
   }
-  for(let r=0; r<mazeRow; r++) {
-    for(let c=0; c<mazeCol; c++) {
-        let top = new Wall()
-        let left = new Wall()
-        if(r != 0) {
-            top = grid[r - 1][c].getWalls()[2]
-        }
-        if(c != 0) {
-            left = grid[r][c - 1].getWalls()[1]
-        }
-        grid[r][c] = new Cell([top, new Wall(), new Wall(), left])
-        for(let i=0; i<4; i++) {
-          grid[r][c].getWalls()[i].addOwner(grid[r][c])
-        }
+  for (let r = 0; r < mazeRow; r++) {
+    for (let c = 0; c < mazeCol; c++) {
+      let top = new Wall();
+      let left = new Wall();
+      if (r != 0) {
+        top = grid[r - 1][c].getWalls()[2];
+      }
+      if (c != 0) {
+        left = grid[r][c - 1].getWalls()[1];
+      }
+      grid[r][c] = new Cell([top, new Wall(), new Wall(), left]);
+      for (let i = 0; i < 4; i++) {
+        grid[r][c].getWalls()[i].addOwner(grid[r][c]);
+      }
     }
   }
-  let maze = []
-  let wallsToDo = []
-  let randomCell = grid[randInt(0, mazeRow - 1)][randInt(0, mazeCol - 1)]
-  maze.push(randomCell)
-  for(let i=0; i<4; i++) {
-      wallsToDo.push(randomCell.getWalls()[i])
+  let maze = [];
+  let wallsToDo = [];
+  let randomCell = grid[randInt(0, mazeRow - 1)][randInt(0, mazeCol - 1)];
+  maze.push(randomCell);
+  for (let i = 0; i < 4; i++) {
+    wallsToDo.push(randomCell.getWalls()[i]);
   }
-  
-  while(maze.length < mazeCol * mazeRow) {
-    let random = Math.floor(Math.random() * (wallsToDo.length - 1))
-    let randomWall = wallsToDo[random]
-    if(randomWall.owner.length > 1 && (!maze.includes(randomWall.owner[0]) || !maze.includes(randomWall.owner[1]))) {
-      randomWall.breakWall()
-      let newOwner = randomWall.owner[!maze.includes(randomWall.owner[0]) ? 0 : 1]
-      maze.push(newOwner)
-      for(let i=0; i<4; i++) {
-        if(newOwner.getWalls()[i].exists) {
-          wallsToDo.push(newOwner.getWalls()[i])
+
+  while (maze.length < mazeCol * mazeRow) {
+    let random = Math.floor(Math.random() * (wallsToDo.length - 1));
+    let randomWall = wallsToDo[random];
+    if (
+      randomWall.owner.length > 1 &&
+      (!maze.includes(randomWall.owner[0]) ||
+        !maze.includes(randomWall.owner[1]))
+    ) {
+      randomWall.breakWall();
+      let newOwner =
+        randomWall.owner[!maze.includes(randomWall.owner[0]) ? 0 : 1];
+      maze.push(newOwner);
+      for (let i = 0; i < 4; i++) {
+        if (newOwner.getWalls()[i].exists) {
+          wallsToDo.push(newOwner.getWalls()[i]);
         }
       }
     }
-    wallsToDo = wallsToDo.filter(wall => wall != randomWall)
-    if(wallsToDo.length == 0) {
-      console.log("Got Stuck")
-      return makeMaze(mazeCol, mazeRow)
+    wallsToDo = wallsToDo.filter(wall => wall != randomWall);
+    if (wallsToDo.length == 0) {
+      console.log("Got Stuck");
+      return makeMaze(mazeCol, mazeRow);
     }
   }
-  console.log("Done Generating")
-  return grid
+  console.log("Done Generating");
+  return grid;
 }
 
 function drawMaze(grid, pS) {
-  document.getElementById("maze").innerHTML = ""
-  for(let r=0; r<grid.length; r++) {
-    let row = document.createElement('div')
-    row.style.cssText = 'display:block;width:' + (pS * grid[0].length) + 'px;height:' + pS + 'px;'
-    document.getElementById("maze").appendChild(row)
-    for(let c=0; c<grid[0].length; c++) {
-      let cell = document.createElement('div')
-      cell.style.cssText = 'display:inline-block;width:' + pS + 'px;height:' + pS + 'px;background:#E3E3E3'
-      if(grid[r][c].getWalls()[0].exists) {cell.style.cssText += 'border-top: 1px solid black'} //top
-      if(grid[r][c].getWalls()[1].exists) {cell.style.cssText += 'border-right: 1px solid black'} //right
-      if(grid[r][c].getWalls()[2].exists) {cell.style.cssText += 'border-bottom: 1px solid black'} //down
-      if(grid[r][c].getWalls()[3].exists) {cell.style.cssText += 'border-left: 1px solid black'} //left
-      row.appendChild(cell)
+  document.getElementById("maze").innerHTML = "";
+  for (let r = 0; r < grid.length; r++) {
+    let row = document.createElement("div");
+    row.style.cssText =
+      "display:block;width:" + pS * grid[0].length + "px;height:" + pS + "px;";
+    document.getElementById("maze").appendChild(row);
+    for (let c = 0; c < grid[0].length; c++) {
+      let cell = document.createElement("div");
+      cell.style.cssText =
+        "display:inline-block;width:" +
+        pS +
+        "px;height:" +
+        pS +
+        "px;background:#E3E3E3";
+      if (grid[r][c].getWalls()[0].exists) {
+        cell.style.cssText += "border-top: 1px solid black";
+      } //top
+      if (grid[r][c].getWalls()[1].exists) {
+        cell.style.cssText += "border-right: 1px solid black";
+      } //right
+      if (grid[r][c].getWalls()[2].exists) {
+        cell.style.cssText += "border-bottom: 1px solid black";
+      } //down
+      if (grid[r][c].getWalls()[3].exists) {
+        cell.style.cssText += "border-left: 1px solid black";
+      } //left
+      cell.setAttribute("onmouseenter", "makePath(this)");
+      row.appendChild(cell);
     }
   }
 }
 
-function newMaze() {
-  let name = document.getElementById("name").value
-  let row = parseInt(document.getElementById("rowNum").value, 10)
-  let col = parseInt(document.getElementById("colNum").value, 10)
-  let s = parseInt(document.getElementById("size").value, 10)
-  if(name == "") {
+function newMaze(id) {
+  let name = document.getElementById("name").value;
+  let row = parseInt(document.getElementById("rowNum").value, 10);
+  let col = parseInt(document.getElementById("colNum").value, 10);
+  let s = parseInt(document.getElementById("size").value, 10);
+  if (name == "") {
     alert("You must enter a name for your maze.");
-  } else if(isNaN(row)) {
+  } else if (isNaN(row)) {
     alert("You must enter a number of rows.");
-  } else if(isNaN(col)) {
+  } else if (isNaN(col)) {
     alert("You must enter a number of columns.");
-  } else if(isNaN(s)) {
+  } else if (isNaN(s)) {
     alert("You must enter a number for passage width.");
   } else {
-    let newMaze = makeMaze(col,row)
-    drawMaze(newMaze, s)
-    document.getElementById("mazeName").innerText = name
-    console.log("Done Drawing") 
+    let newMaze = makeMaze(col, row);
+    drawMaze(newMaze, s);
+    document.getElementById("mazeName").innerText = name;
+    console.log("Done Drawing");
+    fetch("/add", {
+      method: "POST",
+      body: JSON.stringify({
+        id: id,
+        mazeName: name,
+        maze: document.getElementById("maze").innerHTML
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(res => res.json())
+      .then(json => {
+        console.log("Updated!");
+        document.getElementById("maze").innerHTML = json.maze;
+      });
+  }
+}
+
+function deleteMaze(userId) {
+  fetch("/delete", {
+    method: "POST",
+    body: JSON.stringify({
+      _id: document.getElementById("mazeId").innerText.replace("Maze ID: ", "")
+    }),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+    .then(res => res.json())
+    .then(json => {
+      console.log("Deleted!");
+    })
+    .then(fin => populateMazes(userId));
+}
+
+function populateMazes(userId) {
+  showMenu(false);
+  document.getElementById("mazeButtons").innerText = "Loading...";
+  fetch("/getUser", {
+    method: "POST",
+    body: JSON.stringify({ id: userId }),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+    .then(res => res.json())
+    .then(mazes => {
+      document.getElementById("mazeButtons").innerText = "";
+      for (let i = 0; i < mazes.length; i++) {
+        console.log(mazes[i]);
+        let mazeButton = document.createElement("button");
+        mazeButton.textContent = mazes[i].mazeName;
+        mazeButton.style.cssText = "margin: 5px";
+        mazeButton.onclick = function mazeClick() {
+          showMenu(true);
+          document.getElementById("mazeName").innerText = mazes[i].mazeName;
+          document.getElementById("maze").innerHTML = mazes[i].maze;
+          document.getElementById("mazeId").innerText =
+            "Maze ID: " + mazes[i]._id;
+        };
+        document.getElementById("mazeButtons").appendChild(mazeButton);
+      }
+    });
+}
+
+function update(userId) {
+  fetch("/update", {
+    method: "POST",
+    body: JSON.stringify({
+      _id: document.getElementById("mazeId").innerText.replace("Maze ID: ", ""),
+      mazeName: (document.getElementById("newName").value != "" ? document.getElementById("newName").value : document.getElementById("mazeName").innerText),
+      maze: document.getElementById("maze").innerHTML
+    }),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+    .then(res => res.json())
+    .then(json => {
+      console.log(json);
+    })
+    .then(fin => populateMazes(userId));
+}
+
+function showMenu(show) {
+  let display = show ? "block" : "none";
+  let elementIds = [
+    "mazeId",
+    "newName",
+    "update",
+    "delete",
+    "maze",
+    "mazeName"
+  ];
+  for (const el in elementIds) {
+    document.getElementById(elementIds[el]).style.display = display;
+    if (
+      document.getElementById(elementIds[el]).nodeName == "BUTTON" &&
+      display == "block"
+    ) {
+      document.getElementById(elementIds[el]).style.display = "inline-block";
+    }
   }
 }
--- server.js
+++ server.js
@@ -3,23 +3,36 @@
 
 // we've started you off with Express (https://expressjs.com/)
 // but feel free to use whatever libraries or frameworks you'd like through `package.json`.
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
-const passport = require('passport');
-const ensure = require('connect-ensure-login');
+const passport = require("passport");
 const app = express();
-const GitHubStrategy = require('passport-github').Strategy;
+const GitHubStrategy = require("passport-github").Strategy;
 var currentUser = null;
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
+const uri = `mongodb+srv://mazeGeneratorPage:${process.env.DATABASEPASSWORD}@mazes.5kglj.mongodb.net/${process.env.DATABASEID}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
-passport.use(new GitHubStrategy({
-    clientID: process.env.CLIENTID,
-    clientSecret: process.env.SECRET,
-    callbackURL: 'https://a3-margaretearnest.glitch.me/return'
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    return cb(null, profile);
-  }));
+app.use(bodyParser.json({ limit: "10mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
+
+passport.use(
+  new GitHubStrategy(
+    {
+      clientID: process.env.CLIENTID,
+      clientSecret: process.env.SECRET,
+      callbackURL: "https://a3-margaretearnest.glitch.me/return"
+    },
+    function(accessToken, refreshToken, profile, cb) {
+      return cb(null, profile);
+    }
+  )
+);
 
 passport.serializeUser(function(user, cb) {
   cb(null, user);
@@ -32,60 +45,83 @@ passport.deserializeUser(function(obj, cb) {
 app.use(passport.initialize());
 app.use(passport.session());
 
-// app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.get('/',
+app.get("/", function(req, res) {
+  res.render("index", { user: currentUser != null ? currentUser : req.user });
+});
+
+app.get("/login", function(req, res) {
+  res.render("login");
+});
+
+app.get("/login/github", passport.authenticate("github"));
+
+app.get(
+  "/return",
+  passport.authenticate("github", { failureRedirect: "/login" }),
   function(req, res) {
-    res.render('index', { user: req.user });
-  });
+    currentUser = req.user;
+    res.render("index", { user: req.user });
+  }
+);
 
-app.get('/login',
-  function(req, res){
-    res.render('login');
-  });
+app.get("/profile", function(req, res) {
+  res.render("profile", { user: currentUser });
+});
 
-app.get('/login/github',
-  passport.authenticate('github'));
-
-app.get('/return', 
-  passport.authenticate('github', { failureRedirect: '/login' }),
-  function(req, res) {
-    currentUser = req.user
-    res.render('index', { user: req.user });
-  });
-
-app.get('/profile',
-  // ensure.ensureLoggedIn('/login'),
-  function(req, res){
-    res.render('profile', { user: currentUser });
-  });
-
-app.get('/logout', function(req, res){
+app.get("/logout", function(req, res) {
   req.logout();
-  currentUser = null
-  res.redirect('/');
+  delete req.session;
+  currentUser = null;
+  res.redirect("/");
 });
 
 // // make all the files in 'public' available
 // https://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
 
-// // https://expressjs.com/en/starter/basic-routing.html
-// app.get("/", (request, response) => {
-//   response.sendFile(__dirname + "/views/index.html");
-// });
+let collection = null;
+client.connect(err => {
+  collection = client.db("mazeDatabase").collection("mazeCollection");
+});
 
-// // send the default array of dreams to the webpage
-// app.get("/dreams", (request, response) => {
-//   // express helps us take JS objects and send them as JSON
-//   response.json(dreams);
-// });
+app.post("/add", bodyParser.json(), function(req, res) {
+  collection
+    .insertOne(req.body) //req.body is full content of new item
+    .then(dbres => {
+      console.log(dbres);
+      res.json(dbres.ops[0]);
+    });
+});
 
-// app.post("/add", bodyParser.json(), (req, res) => {
-//   dreams.push(req.body.dream)
-//   res.json(req.body)
-// })
+app.post("/delete", bodyParser.json(), function(req, res) {
+  collection
+    .deleteOne({ _id: mongodb.ObjectId(req.body._id) }) //req.body is the _id to be deleted
+    .then(dbres => {
+      res.json(dbres);
+    });
+});
+
+app.post("/getUser", bodyParser.json(), function(req, res) {
+  collection
+    .find({ id: req.body.id }) //req.body is the user id
+    .toArray()
+    .then(dbres => {
+      res.json(dbres);
+    });
+});
+
+app.post("/update", bodyParser.json(), function(req, res) {
+  collection
+    .updateOne(
+      { _id: mongodb.ObjectId(req.body._id) },
+      { $set: { mazeName: req.body.mazeName, maze: req.body.maze } }
+    ) //req.body is whole object, identify by _id
+    .then(dbres => {
+      res.json(dbres);
+    });
+});
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
