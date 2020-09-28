ae252fc90b8b768aac296889666e641c15a4af06
Glitch (a3-afsimoneau) <none>
2020-09-28 19:31:14 +0000

    🎮🐜 Updated with Glitch

--- public/scripts.js
+++ public/scripts.js
@@ -1,2 +1,26 @@
 //client script
 
+const loadTable = function (jsonData) {
+  var table = document.getElementById("resultTableBody");
+  table.innerHTML = "";
+  for (var i = 0; i < jsonData.length; i++) {
+    var tr = table.insertRow(-1);
+    for (var j in jsonData[i]) {
+      var tb = tr.insertCell(-1);
+      tb.innerHTML = jsonData[i][j];
+    }
+  }
+};
+
+window.onload = function(){
+  fetch("/load", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => loadTable(json));
+  const button = document.querySelector("button");
+}
--- server.js
+++ server.js
@@ -1,64 +1,63 @@
 //server script
-
 const express = require("express"),
   mongodb = require("mongodb"),
   bodyparser = require("body-parser"),
   app = express();
 
-app.use(express.static("views/index.html"));
+app.use(express.static("views"))
+app.use(bodyparser.json());
+/**
+const uri =
+  "mongodb+srv://" +
+  process.env.USER +
+  ":" +
+  process.env.PASS +
+  "@" +
+  process.env.HOST +
+  "/UserData" +
+  process.env.DB;
 
-const uri = 'mongodb+srv://'+process.env.USER+':'+process.env.PASS+'@'+process.env.HOST+'/'+process.env.DB
+const client = new mongodb.MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
-const client = new mongodb.MongoClient( uri, { useNewUrlParser: true, useUnifiedTopology:true })
-let collection = null
+let collection = null;
 
-client.connect()
-  .then( () => {
-    // will only create collection if it doesn't exist
-    return client.db( 'UserData' ).createCollection( 'data' )
-  })
-  .then( __collection => {
-    // store reference to collection
-    collection = __collection
-    // blank query returns all documents
-    return collection.find({ }).toArray()
-  })
-  .then( console.log )
-  
-// route to get all docs
-app.get( '/', (req,res) => {
+client.connect().then(__collection => {
+  // store reference to collection
+  collection = client.db("UserData").collection("data");
+  return collection.find({ }).toArray()
+}).then(console.log)
+
+app.use( (req,res,next) => {
   if( collection !== null ) {
-    // get array and pass to res.json
-    collection.find({ }).toArray().then( result => res.json( result ) )
+    next()
+  }else{
+    res.status( 503 ).send()
   }
 })
-  
-app.listen( 3000 )
-
+*/
 app.get("/", (req, res) => {
-  res.sendFile("./views/index.html");//send login page on default
+    res.sendFile(__dirname + "/views/index.html"); //send login page on default
+    console.log("plz2");
 });
 
 app.get("/secure", (req, res) => {
-    res.sendFile("./views/secure.html");//send login page on default
+  res.sendFile(__dirname + "/views/secure.html"); //send login page on default
 });
 
-app.post("/add",(req,res)=>{
+app.post("/add", (req, res) => {});
 
-})
+app.post("/update", (req, res) => {});
 
-app.post("/update",(req,res)=>{
-    
-})
+app.post("/delete", (req, res) => {});
+app.post("/load", (req, res) => {
+  res.writeHead(200,{ 'Content-Type': 'application/json'})
+  //collection.find({}).then((data)=>res.end(JSON.stringify(data)));
+});
 
-app.post("/delete",(req,res)=>{
-    
-})
-
-app.post("/load", (req,res)=>{
-    collection.find({}).toArray().then(result=>res.json(result))
-})
-
-const serverListener = app.listen(process.env.PORT, () => {
-  console.log("Server running on port " + serverListener.address.PORT);
+const listener = app.listen(process.env.PORT, function() {
+  console.log("Your app is listening on port " + listener.address().port);
+  console.log("plz")
 });
