63eeab243ec1158a416cda9ed112b7e071289c0d
Charles Kittler <59940820+cvkittler@users.noreply.github.com>
2020-09-28 13:51:03 -0400

    did alot of stuff

--- public/script.js
+++ public/script.js
@@ -4,15 +4,29 @@
 // define variables that reference elements on our page
 const dreamsList = document.getElementById("dreams");
 const dreamsForm_add = document.getElementById("add-dream");
-const dreamsForm_remove = document.getElementById("remove-dream");
+const dreamsForm_change = document.getElementById("change-dream");
+const loginForm = document.getElementById('login');
 
 // a helper function that creates a list item for a given dream
-function appendNewDream(dream) {
+function appendNewDream(dream, id) {
   const newListItem = document.createElement("li");
+  newListItem.id = id
+  newListItem.onclick = function () {
+    fetch('/delete', {
+      method: "DELETE",
+      body: JSON.stringify({ id }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => response.json())
+      .then(json => {
+        newListItem.remove()
+      })
+  }
   newListItem.innerText = dream;
   dreamsList.appendChild(newListItem);
 }
-
 
 // listen for the form to be submitted and add a new dream when it is
 dreamsForm_add.addEventListener("submit", event => {
@@ -21,39 +35,102 @@ dreamsForm_add.addEventListener("submit", event => {
 
   fetch('/add', {
     method: 'POST',
-    body:JSON.stringify({dream:dreamsForm_add.elements.dream.value }),
-    headers:{
-      "Content-Type":"application/json"
+    body: JSON.stringify({ dream: dreamsForm_add.elements.dream.value }),
+    headers: {
+      "Content-Type": "application/json"
     }
   })
-  .then(response => response.json())
-  .then(json => {
-    appendNewDream( json.dream )
-  })
+    .then(response => response.json())
+    .then(json => {
+      console.log('responds dream:' + json.dream)
+      if (json.dream === 'already exists') {
+        console.log('Element already exists')
+        alert("You already have this dream");
+      } else {
+        appendNewDream(json.dream, json._id)
+      }
+    })
 
   // reset form
   dreamsForm_add.reset();
   dreamsForm_add.elements.dream.focus();
 });
 
-//listen for the from to have a delete requenst
-dreamsForm_remove.addEventListener('/delete', event => {
+// listen for the form to be submitted and add a new dream when it is
+dreamsForm_change.addEventListener("submit", event => {
   // stop our form submission from refreshing the page
   event.preventDefault();
 
-  fetch('/delete', {
-    method: 'DELETE'
+  fetch('/change', {
+    method: 'PUT',
+    body: JSON.stringify({
+      id: dreamsForm_change.elements.old_dream.value,
+      name: dreamsForm_change.elements.new_dream.value
+    }),
+    headers: {
+      "Content-Type": "application/json"
+    }
   })
+    .then(response => response.json()
+      .then(json => {
+
+      }))
+
+  // reset form
+  dreamsForm_change.reset();
+  dreamsForm_change.elements.old_dream.focus();
+});
+
+loginForm.addEventListener("submit", event => {
+  event.preventDefault();
+  fetch('/login', {
+    method: 'POST',
+    body: JSON.stringify({
+      username: loginForm.elements.username.value,
+      password: loginForm.element.password.value
+    }),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+    .then(response => response.json())
+    .then(json => {
+      console.log('responds dream:' + json.dream)
+      if (json.dream === 'already exists') {
+        console.log('Element already exists')
+        alert("You already have this dream");
+      } else {
+        appendNewDream(json.dream, json._id)
+      }
+    })
 
 })
 
 // fetch the initial list of dreams
 fetch("/dreams")
-.then(response => response.json()) // parse the JSON from the server
-.then(dreams => {
-  // remove the loading text
-  dreamsList.firstElementChild.remove();
+  .then(response => response.json())
+  // parse the JSON from the server
+  .then(data => {
+    console.log("Data: " + data)
+    paresedData = JSON.parse(data)
 
-  // iterate through every dream and add it to our page
-  dreams.forEach(appendNewDream);
-});
+    // remove the loading text
+    dreamsList.firstElementChild.remove();
+
+    // iterate through every dream and add it to our page
+    paresedData.forEach(element => {
+      appendNewDream(element.dream, element._id)
+    });
+  });
+
+function getInputsByValue(value) {
+  var allInputs = document.getElementsByTagName("dreams");
+  var results = [];
+  for (var x = 0; x < allInputs.length; x++)
+    if (allInputs[x].value == value) {
+      console.log(allInputs[x])
+      return (allInputs[x].id);
+    } else {
+      console.log('test')
+    }
+}
--- server.js
+++ server.js
@@ -4,46 +4,93 @@
 // we've started you off with Express (https://expressjs.com/)
 // but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require("express");
-const body_parser = require("body-parser");
+const body_parser = require("body-parser")
+var timeout = require('connect-timeout')
+var responseTime = require('response-time')
+var morgan = require('morgan')
 const app = express();
 
-// our default array of dreams
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
+//database stuff
+let db = null
+const mongodb = require('mongodb');
+const { request, response } = require("express");
+const MongoClient = mongodb.MongoClient
+const uri = "mongodb+srv://user:Redsox99@cluster0.ocd5q.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  db = client.db("Dreams")
+})
 
 // make all the files in 'public' available
 // https://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
+app.use(express.static("public"))
+app.use(haltOnTimedout)
+app.use(responseTime({
+  digits: 2
+}))
 
-// https://expressjs.com/en/starter/basic-routing.html
+// server homepage
 app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
+  response.sendFile(__dirname + "/views/login.html");
 });
+
+app.post('/login', timeout('5s'), body_parser.json(), haltOnTimedout, (request, response) => {
+  if(request.body.username === "username" && request.body.password === "password"){
+    response.sendFile(__dirname + "/views/index.html");
+  }
+})
+
+// log error responses
+morgan('combined', {
+  skip: function (req, res) { return res.statusCode < 400 }
+})
+
+app.use(responseTime((req, res, time) => {
+  console.log(`${req.method} ${req.url}` + "Response Time: " + `${time}` + 'ms');
+}))
 
 // send the default array of dreams to the webpage
 app.get("/dreams", (request, response) => {
   // express helps us take JS objects and send them as JSON
-  response.json(dreams);
+  db.collection("test").find({}).toArray()
+    .then(result => {
+      response.json(JSON.stringify(result))
+      console.log(result)
+    });
 });
 
 //add the data to the database
-app.post('/add', body_parser.json(), (request,response) => {
-  // the request gets added to the database
-  console.log('Adding to the database: ' + request.body.dream)
-  dreams.push(JSON.stringify(request.body.dream))
-  response.json(request.body)
+app.post('/add', timeout('5s'), body_parser.json(), haltOnTimedout, (request, response) => {
+  // the request gets added to the local database
+  // add the element to the remote database
+  db.collection("test").insertOne(request.body)
+    .then(dbRespons => {
+      response.json(dbRespons.ops[0])
+    })
 })
 
 // remove data from the database
-app.delete('/delete', body_parser.json(), (request,response) => {
-  // the request gets reomoved from the database
-  console.log('Removing from the database')
+app.delete('/delete', timeout('5s'), body_parser.json(), haltOnTimedout, (request, response) => {
+  console.log(request.body.id)
+  db.collection("test")
+    .deleteOne({ _id: mongodb.ObjectID(request.body.id) })
+    .then(results => response.json(results))
+})
+
+app.put('/change', timeout('5s'), body_parser.json(), haltOnTimedout, (request, response) => {
+  db.collection("test").updateOne(
+    { dream: request.body.id },
+    { $set: { dream: request.body.name } }
+  )
+    .then(result => response.json(result))
 })
 
 // listen for requests :)
 const listener = app.listen(3000, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
+
+//timeout handler
+function haltOnTimedout(req, res, next) {
+  if (!req.timedout) next()
+}
