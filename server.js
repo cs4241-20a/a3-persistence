fab30099ca1ba6e436976e9fad99faaa5ef1fb07
ikyriazis <ikyriazis@wpi.edu>
2020-09-28 17:00:44 -0400

    adding admin page

--- server.js
+++ server.js
@@ -5,7 +5,10 @@
 // but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require("express");
 const passport = require("passport");
+const session = require("express-session");
+const bodyParser = require("body-parser");
 const LocalStrategy = require("passport-local").Strategy;
+const flash = require('connect-flash');
 const app = express();
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
@@ -16,33 +19,59 @@ client.connect(err => {
   users = client.db("test").collection("users");
   // perform actions on the collection object
 });
-
+let user = null;
 
 // make all the files in 'public' available
 // https://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
 app.use(express.json());
+app.use(session({ secret: "cats" }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+      users.findOne({ username: username }, async function(err, user) {
+        if (err) { return done(err); }
+        let doesUserExist = await userExists(username);
+        if (!doesUserExist) {
+          return done(null, false, { message: 'Incorrect username.' });
+        }
+        let actualPassword = await getPassword(username);
+        if (actualPassword !== password) {
+          return done(null, false, { message: 'Incorrect password.' });
+        }
+        console.log(user);
+        return done(null, user);
+      });
+    }
+));
+
+passport.serializeUser(function(user, done) {
+  console.log(user._id);
+  done(null, user._id);
+});
+
+passport.deserializeUser(function(id, done) {
+  users.findOne({_id: id}, function(err, user) {
+    done(err, user);
+  });
+});
 
 // https://expressjs.com/en/starter/basic-routing.html
 app.get("/", (request, response) => {
   response.sendFile(__dirname + "/views/index.html");
 });
 
-passport.use(new LocalStrategy(
-    function(username, password, done) {
-      users.findOne({ user: username }, function(err, user) {
-        if (err) { return done(err); }
-        if (!userExists(username)) {
-          return done(null, false, { message: 'Incorrect username.' });
-        }
-        if (getPassword(username) !== password) {
-          return done(null, false, { message: 'Incorrect password.' });
-        }
-        return done(null, user);
-      });
-    }
-));
-
+app.get("/getusername", (request, response) => {
+  console.log("trying for username");
+  if (user !== null) {
+    console.log("sending json");
+    response.json({username: user});
+  }
+})
 
 app.post("/register", (request, response) => {
   users.findOne({username: request.body.username}).then(result => {
@@ -56,11 +85,33 @@ app.post("/register", (request, response) => {
   })
 });
 
-// app.post('/login',
-//     passport.authenticate('local', { successRedirect: '/',
-//       failureRedirect: '/login',
-//       failureFlash: true })
-// );
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+app.post('/login', async function (req, res){
+  passport.authenticate('local', { successRedirect: '/getusername',
+    failureRedirect: '/login',
+    failureFlash: true })
+  let userData = req.body;
+  let username = userData.username;
+  let password = userData.password;
+  let isActualUser = await userExists(username);
+  if (isActualUser) {
+    let actualPassword = await getPassword(username);
+    if (actualPassword === password) {
+      console.log("user logged in");
+      user = username;
+    }
+    else {
+      res.sendStatus(500);
+    }
+  } else {
+    res.sendStatus(500);
+  }
+  return res.end();
+});
 
 // listen for requests :)
 const listener = app.listen(5000, () => {
@@ -69,12 +120,11 @@ const listener = app.listen(5000, () => {
 
 async function userExists(username) {
   // checks to see if the username exists in the database
-  users.findOne({username: username}).then(result => {
-    console.log(result);
-    return result !== null;
-  })};
+  let result = await users.findOne({username: username});
+  return result !== null;
+};
 
 async function getPassword(username) {
-  users.findOne({username: username}).then(result => {
-    return result.password;
-})};
+  let result = await users.findOne({username: username});
+  return result.password;
+};
