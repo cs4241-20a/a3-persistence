7c7e1af85d502e287306ca3d5429aa5abc267f38
Glitch (opinel98-a3-persistence) <none>
2020-09-29 03:46:18 +0000

    💰🛎 Updated with Glitch

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -4,18 +4,118 @@
 console.log("hello world :o");
 
 // define variables that reference elements on our page
-const dreamsList = document.getElementById("dreams");
-const dreamsForm = document.querySelector("form");
+const memberTable = document.getElementById("membersB");
+const Form = document.querySelector("form");
 
-// a helper function that creates a list item for a given dream
-function appendNewDream(dream) {
-    const newListItem = document.createElement("li");
-    newListItem.innerText = dream;
-    dreamsList.appendChild(newListItem);
+function clearTable() {
+    var table = document.getElementById("membersB");
+    for(var i = table.rows.length - 1; i > 0; i--){
+      table.deleteRow(i);
+    }
 }
 
+function addEntryToTable(jsonEntry) {
+    //Find the table
+    const tableBody = document.getElementById("membersB");
+    //Insert a new row and then new cells
+    let row = tableBody.insertRow(-1);
+    let cell1 = row.insertCell(0);
+    let cell2 = row.insertCell(1);
+    let cell3 = row.insertCell(2);
+    let cell4 = row.insertCell(3);
+    let cell5 = row.insertCell(4);
+
+    //Fill those cells with the data gotten back from the server, this includes the new fields
+    cell1.innerHTML = jsonEntry.nickname;
+    cell2.innerHTML = jsonEntry.distance;
+    cell3.innerHTML = jsonEntry.time;
+    cell4.innerHTML = jsonEntry.date;
+    cell5.innerHTML = jsonEntry.description;
+}
+
+// //fetch the initial form input
+// fetch("/loadAll")
+//   .then(response => response.json())
+//   .then(entries => {
+//     console.log(entries)
+//   console.log("on load")
+//     entries.forEach(e => addEntryToTable(e))
+// });
+
+//submit the new entry to the server and get the response to load on the table
+const submit = function(event) {
+  event.preventDefault();
+  
+  let entry = {
+            nickname: document.querySelector( '#nickname').value,
+            distance: document.querySelector( '#distance').value,
+            time: document.querySelector( '#time').value,
+            date: document.querySelector( '#date').value,
+            description: document.querySelector( '#description').value
+          }
+  
+  
+  if(entry.nickname === ""|| entry.distance === "" || entry.time === "" || entry.date === ""){
+        window.alert("All fields except for the description must be inputed");
+        return false;
+    }
+  
+  /*if(!parseDate(entry.dateofbirth)){
+    window.alert("The date of birth must be in MM/DD/YYYY format");
+    return false;
+  }*/
+  
+  let body = JSON.stringify(entry);
+  console.log(body)
+  fetch('/add', {
+    method: 'POST',
+    body,
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(response => response.json())
+  .then(json =>{
+    console.log(json);
+    //console.log("here");
+    addEntryToTable(json);
+  })
+};
+
+
+// const logOut = function(event){ 
+//   event.preventDefault();
+//   fetch('/logout')
+//   .then(response =>{
+//     console.log("logged out")
+//     window.location.href= "
+//   })
+// }
+
+const deleteAll = function(event){
+  var r = confirm("Are you sure you want to delete everything?\nAll your data will be lost.");
+  if(r == true){
+    fetch('/deleteAll', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+  }
+}
+
+window.onload = function() {
+  const submB = document.getElementById('submitB')
+  const delB = document.getElementById('deleteB')
+  const logout = document.getElementById('logOut')
+//  logout.onclick = logOut;
+  delB.onclick = deleteAll;
+  submB.onclick = submit;
+}
+
+
 // fetch the initial list of dreams
-fetch("/dreams")
+/*fetch("/dreams")
     .then(response => response.json()) // parse the JSON from the server
     .then(dreams => {
         // remove the loading text
@@ -38,4 +138,4 @@ fetch("/dreams")
             dreamsForm.reset();
             dreamsForm.elements.dream.focus();
         });
-    });
+    });*/
--- server.js
+++ server.js
@@ -6,12 +6,25 @@
 const express = require("express");
 const app = express();
 
-// our default array of dreams
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
+let collection = null
+let users = null
+let name = "3"
+const mongodb = require('mongodb')
+const bodyparser = require('body-parser')
+const mongoose = require('mongoose')
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://opinel98:Alarma22@cluster0.luw3v.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+app.use(bodyparser.json())
+client.connect(err => {
+  console.log("DB conected");
+  collection = client.db("table1").collection("Members_Log");
+  //client.close();
+});
+//const mongodb = require('mongodb');
+
+
+
 
 // make all the files in 'public' available
 // https://expressjs.com/en/starter/static-files.html
@@ -22,14 +35,27 @@ app.get("/", (request, response) => {
   response.sendFile(__dirname + "/views/index.html");
 });
 
+app.post('/add',bodyparser.json(),function(request, response){
+  console.log(" in add function")
+  collection.insertOne({nickname: request.body.nickname, distance: request.body.distance, time: request.body.time, date: request.body.date, description: request.body.description})
+  .then(dbresponse => {
+    response.json(dbresponse.ops[0]);
+  })
+})
+
 // send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
-  // express helps us take JS objects and send them as JSON
-  response.json(dreams);
+app.get("/loadAll", (request, response) => {
+  if(collection !== null){
+    collection.find({nickname: name}).toArray().then(result => {
+      response.json(result)
+    })
+  }
 });
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
+
+
 
