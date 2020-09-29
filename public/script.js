fc784fe0ec9b9968211e3d3fab09aed2408d4d2c
ikyriazis <ikyriazis@wpi.edu>
2020-09-29 15:35:11 -0400

    Cleaning up

--- public/script.js
+++ public/script.js
@@ -1,41 +1,83 @@
-// client-side js, loaded by index.html
-// run by the browser each time the page is loaded
+let postTitles = [];
+async function loadDB() {
+    let posts = document.getElementById("post-container");
+    posts.innerHTML = "";
+    await fetch('/loadData').then(result => result.json().then(result => {
+        result.forEach((post) => {
+            postTitles.push(post.title);
+            posts.innerHTML += `
+        <div align="center" class="post">
+        <h3>${post.title}</h3>
+        <img height = "400px" width = "400px" src="${post.image}">
+        <div class="buyinginfo"><p>This item hasn't been sold yet</p> <div></div>
+        <p>${post.price}</p></div>
+        <div style="margin-top: 20px;" id="${post.title}commentsection"><button id="viewcomments${post.title}">View comments</button></div>
+        <div class="addcomment"><div></div><input type="text" id="${post.title}commentbox" placeholder="Add a comment..."><button id="${post.title}commentbutton">Submit</button> <div></div></div>
+        </div>
+        `
+        })
+    }));
 
-console.log("hello world :o");
-
-// define variables that reference elements on our page
-const dreamsList = document.getElementById("dreams");
-const dreamsForm = document.querySelector("form");
-
-// a helper function that creates a list item for a given dream
-function appendNewDream(dream) {
-  const newListItem = document.createElement("li");
-  newListItem.innerText = dream;
-  dreamsList.appendChild(newListItem);
 }
+window.onload = () => loadDB().then(result => {
+    let commentButtons = [];
+    let postCommentButtons = [];
+    let commentTextBoxes = [];
+    for (let i = 0; i < postTitles.length; i++) {
+        commentButtons[i] = document.getElementById('viewcomments' + postTitles[i]);
+        postCommentButtons[i] = document.getElementById(postTitles[i] + 'commentbutton');
+        commentTextBoxes[i] = document.getElementById(postTitles[i] + 'commentbox');
+        // submitting comment
+        postCommentButtons[i].addEventListener('click', function () {
+            fetch('/addcomment', {
+                method: "POST",
+                body: JSON.stringify({title: postTitles[i], text: commentTextBoxes[i].value}),
+                headers: {'Content-Type': 'application/json'}
+            })
+        });
+        // the show comments button
+        commentButtons[i].addEventListener('click', function () {
+            let commentSection = document.getElementById(postTitles[i] + 'commentsection');
+            commentSection.innerHTML = "";
+            fetch('/loadcomments', {
+                method: "POST",
+                body: JSON.stringify({title: postTitles[i]}),
+                headers: {'Content-Type': 'application/json'}
+            }).then(result => result.json()).then(result => {
+                result.forEach((comment) => {
+                    let pElement = document.createElement("p");
+                    pElement.innerText = comment.text;
+                    commentSection.appendChild(pElement);
+                })
+            })
+        })
+    }
+});
 
-// fetch the initial list of dreams
-fetch("/dreams")
-  .then(response => response.json()) // parse the JSON from the server
-  .then(dreams => {
-    // remove the loading text
-    dreamsList.firstElementChild.remove();
-  
-    // iterate through every dream and add it to our page
-    dreams.forEach(appendNewDream);
-  
-    // listen for the form to be submitted and add a new dream when it is
-    dreamsForm.addEventListener("submit", event => {
-      // stop our form submission from refreshing the page
-      event.preventDefault();
-
-      // get dream value and add it to the list
-      let newDream = dreamsForm.elements.dream.value;
-      dreams.push(newDream);
-      appendNewDream(newDream);
-
-      // reset form
-      dreamsForm.reset();
-      dreamsForm.elements.dream.focus();
-    });
-  });
+fetch("/getusername", {
+    method: "GET"
+}).then((result) => {
+    document.getElementById("username").innerHTML = result.username;
+});
+const register = document.getElementById("register");
+const login = document.getElementById("login");
+let user = document.getElementById("user");
+const pass = document.getElementById("pass");
+register.addEventListener("click", function () {
+    fetch("/register", {
+        method: "POST",
+        body: JSON.stringify({username: user.value, password: pass.value}),
+        headers: {'Content-Type': 'application/json'}
+    })
+});
+login.addEventListener("click", function () {
+    fetch("/login", {
+        method: "POST",
+        body: JSON.stringify({username: user.value, password: pass.value}),
+        headers: {'Content-Type': 'application/json'}
+    }).then((result) => {
+        fetch('/getusername')
+            .then(response => response.json())
+            .then(json => document.getElementById("username").innerHTML = json.username);
+    })
+});
