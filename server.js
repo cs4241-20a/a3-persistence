78f777d877fec9a2e5217ee5ca44371de2e79d2d
seungho715 <58754121+seungho715@users.noreply.github.com>
2020-09-28 22:55:33 -0400

    one

--- server.js
+++ server.js
@@ -5,15 +5,12 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const favicon = require('serve-favicon');
 const responseTime = require('response-time');
-const compression = require('compression');
+const helmet = require('helmet');
 const app = express(),
 const port = 3000;
-const passport = require('passport')
-const LocalStrategy = require('passport-local').Strategy;
 
 app.use(express.static("public"));
 app.use(bodyParser.json());
-app.use(compression());
 app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
 app.use(
     responseTime((request, response, time) =>
@@ -24,6 +21,8 @@ app.use(morgan("tiny"));
 app.use(helmet());
 require("dotenv").config();
 const mongodb = require("mongodb");
+let client_id = process.env.GITHUB_CLIENT_ID;
+let client_secret = process.env.GITHUB_CLIENT_SECRET;
 
 //connectng to mongodb
 const MongoClient = mongodb.MongoClient;
@@ -36,112 +35,125 @@ client.connect(err => {
     // perform actions on the collection object
 });
 
-passport.use(new LocalStrategy(
-    function (username, userPassword, done) {
-        const userColl = client.db("bilbo").collection("william");
-        userColl.find({ username: username, password: userPassword }).toArray()
-            .then(function (result) {
-                if (result.length >= 1) {
-                    return done(null, username);
-                } else {
-                    return done(null, false, { message: "Incorrect username or password" });
-                }
+
+app.get("/", (request, response) => {
+    if (request.session.githubid) {
+        response.sendFile(__dirname + "/public/index.html");
+    } else {
+        response.sendFile(__dirname + "/public/login.html");
+    }
+});
+
+app.get("/geturl", (request, response) => {
+    const path = request.protocol + "://" + request.get("host");
+    const url =
+        "https://github.com/login/oauth/authorize?client_id=${client_id}&redirect_uri=${path}/login/github/callback";
+    response.json(url);
+});
+
+async function getAccessToken(code) {
+    const response = await fetch("https://github.com/login/oauth/access_token", {
+        method: 'POST',
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            client_id,
+            client_secret,
+            code
+        })
+    });
+    const data = await response.text();
+
+    const params = new URLSearchParams(data);
+    return params.get('access_token');
+}
+
+async function getGHUser(accessToken) {
+    const request = await fetch("https://api.github.com/user", {
+        headers: { Authorization: `bearer ${accessToken}` }
+    });
+    const data = await request.json();
+    return data;
+}
+
+app.get('/login/github/callback', async (request, response) => {
+    const accessToken = await getAccessToken(
+        request.query.code,
+        client_id,
+        client_secret
+    );
+    const GHData = await getGHUser(accessToken);
+
+    if (GHData) {
+        request.session.githubid = GHData.id;
+        request.session.token = GHData.token;
+        response.redirect("/");
+    } else {
+        console.log("Error while logging in");
+        response.redirect("/login.html");
+    }
+});
+
+app.get("/", (request, response) => {
+    request.session = null;
+    response.redirect("/");
+});
+
+app.post("/submit", (request, response) => {
+    const json = {
+        githubid: request.session.githubid,
+        studentName: request.body.studentName,
+        studentID: request.body.studentID,
+        studentClass: request.body.studentClass,
+        timeWorked: request.body.timeWorked,
+        payment: request.body.payment
+    };
+    collection.insertOne(json).then(data => {
+        response.json(data.json[0]);
+    });
+});
+
+app.post("/delete", (request, response) => {
+    collection.deleteOne({ _id: mongodb.ObjectID(request.body._id) }).then(() => {
+        var array = [];
+        collection
+            .find({ "githubid": request.session.githubid })
+            .forEach(doc => {
+                array.push(doc);
+            })
+            .then(() => {
+                response.json(array);
             });
-    }
-));
-
-passport.serializeUser(function (user, done) {
-    done(null, user);
+    });
 });
 
-passport.deserializeUser(function (user, done) {
-    done(null, user);
+app.post("/edit", (request, response) => {
+    const json = {
+        githubid: request.session.githubid,
+        studentName: request.body.studentName,
+        studentID: request.body.studentID,
+        studentClass: request.body.studentClass,
+        timeWorked: request.body.timeWorked,
+        payment: request.body.payment
+    };
+    const id = request.body._id;
+    const newVal = { $set: json };
+    collection.updateOne(
+        { _id: mongodb.ObjectID(request.body._id) },
+        newVal,
+        (error, response) => {
+            if (error) throw error;
+            return;
+        }
+    );
+    var array = [];
+    collection
+        .find({ githubid: request.session.githubid })
+        .forEach(doc => {
+            array.push(doc);
+        })
+        .then(() => {
+            response.json(array);
+        });
 });
-
-app.post("/submit", bodyParser.json(), function (request, response) {
-    //write post request code for a new item here
-    let data = request.body;
-
-    collection.insertOne(data, function (err, obj) {
-        if (err) {
-            response.sendStatus(500);
-        } else {
-            response.json(data);
-        }
-    })
-})
-
-app.post("/update", bodyParser.json(), function (request, response) {
-    //Editing items here
-    let keys = Object.keys(request.body);
-    let temp = {};
-    for (let i = 0; i < keys.length; i++) {
-        const element = keys[i];
-        if (element !== "id") {
-            temp[element] = request.body[element];
-        }
-    }
-
-    collection.updateOne({ _id: new mongodb.ObjectID(request.body.id) }, { $set: temp }, function (err, obj) {
-        if (err) {
-            response.sendStatus(500);
-        } else {
-            response.sendStatus(200);
-        }
-    })
-})
-
-app.post("/delete", bodyParser.json(), function (request, response) {
-    //Deleted items here
-    collection.deleteOne({ _id: new mongodb.ObjectID(request.body.id) }, function (err, obj) {
-        if (err) {
-            response.sendStatus(500);
-        } else {
-            response.sendStatus(200);
-        }
-    })
-})
-
-app.post("/data", bodyParser.json(), function (request, response) {
-    //Get all user data
-    collection.find({ username: request.body.username }).toArray().then(result => response.json(result));
-})
-
-app.post("/newuser", bodyParser.json(), function (request, response) {
-    //Creating new user account
-    const userColl = client.db("bilbo").collection("william");
-    userColl.insertOne(request.body)
-        .then(() => response.sendStatus(200));
-})
-
-app.post('/login', bodyParser.json(),
-    passport.authenticate('local', { failureFlash: false }), function (request, response) {
-        response.json({ username: request.user });
-    }
-);
-
-
-let server = null;
-waitForElement();
-
-//Wait for the database connection to be made before the server
-function waitForElement() {
-    if (collection !== null) {
-        console.log("Server started");
-        server = app.listen(process.env.PORT || 3000);
-    }
-    else {
-        setTimeout(waitForElement, 500);
-    }
-}
-
-process.on('SIGTERM', shutDown);
-process.on('SIGINT', shutDown);
-
-function shutDown() {
-    console.log("Shutting down");
-    client.close();
-    server.close()
-}
 
 app.listen(process.env.PORT || port)
