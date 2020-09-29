f3487d5f0d6bc95f6db00b15849d0b73dad8a85f
Dev Patel <46830802+dev4848@users.noreply.github.com>
2020-09-28 23:52:53 -0400

    functionality working except database changes

--- server.js
+++ server.js
@@ -7,23 +7,12 @@ const express = require('express'),
       port = 3000,
       app = express()
 
-// app.use(express.static("public"));
+app.use(express.static("public"));
 
- app.get('/', (req, res) => {
-   res.sendFile(__dirname + '/public/login.html')
- })
+ // app.get('/', (req, res) => {
+ //   res.sendFile(__dirname + '/public/login.html')
+ // })
 
-
-const MongoClient = mongodb.MongoClient;
-const uri = "mongodb+srv://dev4488:meow4444@cluster0.ouuiy.mongodb.net/<dbname>?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-client.connect(err => {
-  const collection = client.db("sample_airbnb").collection("listingsAndReviews");
-  // console.log(collection)
-  // perform actions on the collection object
-  client.close();
-});
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Example app listening at http://localhost:${port}`)
@@ -31,6 +20,8 @@ app.listen(process.env.PORT || 3000, () => {
 
 app.use(passport.initialize());
 app.use(passport.session());
+
+let username = null;
 
 passport.serializeUser(function(user, cb) {
   cb(null, user);
@@ -67,37 +58,116 @@ const clientSecret = "5df94e2a06f11fd8ec8881945d0be0c63d404028";
 let accessToken = "";
 
 app.get('/auth/github/callback',
-  passport.authenticate('github', { failureRedirect: '/login' }),
+  // passport.authenticate('github', { failureRedirect: '/login' }),
   function (req, res) {
-    // Successful authentication, redirect home.
-    // res.redirect('/public/index.html');
-    res.sendFile(__dirname + '/public/index.html')
-  });
-
-// app.get("/auth/github/callback", (req, res) => {
-//     res.sendFile(__dirname + "/public/index.html");
-//     const requestToken = req.query.code;
-//     axios({
-//         method: "post",
-//         url: `https://github.com/login/oauth/access_token?client_id=${clientID}&client_secret=${clientSecret}&code=${requestToken}`,
-//         headers: {
-//           accept: "application/json"
-//         }
-//     })
-//     .then(res => {
-//           fetch("https://api.github.com/user", {
-//             headers: {
-//               Authorization: "token " + accessToken
-//             }
-//           })
-//           .then(res => res.json())
-//           .then(res => {
-//                 username = res.login;
-//           });
-//     });
-// });
+    res.sendFile(__dirname + '/public/logins.html')
+    const requestToken = req.query.code;
+    axios({
+        method: "post",
+        url: `https://github.com/login/oauth/access_token?client_id=${clientID}&client_secret=${clientSecret}&code=${requestToken}`,
+        headers: {
+          accept: "application/json"
+        }
+      })
+        .then(response => {
+          accessToken = response.data.access_token;
+          console.log(response.data)
+          console.log(accessToken)
+          return accessToken;
+        })
+        .then(res => {
+          fetch("https://api.github.com/user", {
+            headers: {
+              Authorization: "token " + accessToken
+            }
+          })
+            .then(res => res.json())
+            .then(res => {
+              console.log(res.login)
+              console.log("that was login")
+              username = res.login;
+            });
+        });
+    });
 
 app.get("/error", function(err, req, res, next) {
   console.error(err.stack);
   res.status(500).send("Login Error");
 });
+
+
+// Database
+
+const MongoClient = mongodb.MongoClient;
+const uri = "mongodb+srv://dev4488:meow4444@cluster0.ouuiy.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+let collection = null;
+
+client.connect(err => {
+  collection = client.db("dataset").collection("a3");
+  // console.log(collection)
+});
+
+// var addFunc = function(request, response, next) {
+//     let json = request.body
+
+//     json.name = name
+//     json.subject = subject
+//     json.date = date
+
+//     let tempdata = []
+//     tempdata.push(json)
+//     request.json = tempdata
+//     next()
+// }
+
+app.get('/data', function(req, res) {
+    console.log("Trying to get data from DB")
+
+    collection.find({ user: username}).toArray().then(result => res.json(result))
+})
+
+app.post("/add", bodyparser.json(), function(req, res) {
+      console.log("adding")
+      console.log(req.body)
+      collection.insertOne(req.body)
+              .then(dbresponse => {
+                  console.log(dbresponse)
+                  res.json(dbresponse.ops)
+              })
+              .then(json => function(req, res) {
+                  res.writeHead(200, "OK", { 'Content-Type': 'application/json' })
+                  res.write(JSON.stringify(json))
+                  res.end()
+              })
+});
+
+app.delete('/delete', bodyparser.json(), function(req, res) {
+    console.log("Delete.")
+    console.log(req.body.id)
+    collection.deleteOne({ id: req.body.id })
+        .then(result => res.json(result))
+        .then(json => function(req, res) {
+            res.writeHead(200, "OK", { 'Content-Type': 'application/json' })
+            res.write(JSON.stringify(json))
+            res.end()
+        })
+})
+
+app.put('/put', bodyparser.json(), function(req, res) {
+    console.log("Updating")
+    collection
+        .updateOne({ id: req.body.id },
+        {$set: {
+                name: req.body.name,
+                subject: req.body.subject,
+                date: req.body.date,
+            }
+        })
+        .then(result => res.json(result))
+        .then(json => function(req, res) {
+            res.writeHead(200, "OK", { 'Content-Type': 'application/json' })
+            res.write(JSON.stringify(json))
+            res.end()
+        })
+})
