9b57eb02ed3f9f1c82aaf90ba2788cedee810a7f
Isabel <imoralessirgo@outlook.com>
2020-10-01 00:05:52 -0400

    adding auth

--- server.js
+++ server.js
@@ -1,12 +1,54 @@
-const express = require('express'),
-    app = express()
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var upload = multer();
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
 
-app.use(function (req,rest,next){
-    console.log(req.url)
-    next()
-})
+app.set('view engine', 'pug');
+app.set('views','./views');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(upload.array());
+app.use(cookieParser());
+app.use(session({secret: "Your secret key"}));
+
+var Users = [];
 
 
-app.use(express.static('./'))
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://dbUser:dbPassword@cluster0.ui701.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+    const collection = client.db("a3").collection("users");
+    // perform actions on the collection object
+    client.close();
+});
 
-app.listen(3000)
+
+
+app.get('/login', function(req, res){
+    res.render('login');
+});
+
+app.post('/login', function(req, res){
+    if(!req.body.id || !req.body.password){
+        res.status("400");
+        res.send("Invalid details!");
+    } else {
+        Users.filter(function(user){
+            if(user.id === req.body.id){
+                res.render('signup', {
+                    message: "User Already Exists! Login or choose another user id"});
+            }
+        });
+        var newUser = {id: req.body.id, password: req.body.password};
+        Users.push(newUser);
+        req.session.user = newUser;
+        res.redirect('/index.html');
+    }
+});
+
+app.listen(3000);
