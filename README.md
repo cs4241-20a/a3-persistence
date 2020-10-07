875266f82d3e12db732a1424c1ab5adac4cab26e
Glitch (hezi-k-a3) <none>
2020-10-07 10:22:43 +0000

    🐣🚛 Updated with Glitch

--- README.md
+++ README.md
@@ -1,30 +1,31 @@
-# Assignment 2 - Short Stack: Basic Two-tier Web Application using HTML/CSS/JS and Node.js
+# Assignment 3 - Persistence: Two-tier Web Application with Database, Express server, and CSS template
 ---
 
-https://a2-hezi-k.glitch.me  
+https://glitch.com/~hezi-k-a3
 
 ## WPI Football
 
-WPI Football seeks to address the need of organising pick up football in WPI. Currently the system is set up on messenger. Someone will send a message asking to play and people respond accordingly. However the system can be confusing and convoluted. I would really like to develop this website further. As a starter, I wanted to create a system where other players can check eachother out and see simple things like their name and their stats. I wanted to make this on a separate page however I could not figure out how to do this with the sever. You can see the commented out code in the respective files. I would like to make this more robust and let people add more stats or even profile pictures. For now a user is able to put in their username, password and some basic stats and this is displayed in a table below. However, I think this has a lot of potential.
+WPI Football is a simple website that allows users to login and manage a team sheet. This is an updated version of the A2 project. 
 
-One other future I could not figure out was how to delete an entry, with a button. However, I think I need to spend more time on this.
+## Baseline Requirements
 
-## Technical Achievements
+- **Node.js server:** Built a server that relies on the express framework to handle communication between the client and the server. The server connects to MongoDB to store user information
+- **Results:** When the user submits the form, their entry is reflected in the results table to the right
+- **Form Entry:** The user is able to add, edit and delete data by using the appropiate buttons 
+- **Middleware Packages:**
+  * Passport: Used to implement secure authentication via the github login strategy
+  * Helmet: Used to add HTTP headers to improve security of the website
+  * Response Time: Used to track the response time of every request
+  * Morgan: Logs the requests to the terminal, this was very helpful when debugging
+  * Body Parser: Used to parse all the bodies in the requests that the client received
+- **Persistent Data Storage:** When the user enters data, logs out or reloads the page their data will still be there. This was achieved with MongoDB
+- **Use of CSS Framework:** I used 2 CSS frameworks. MUI was used for the buttons on the login page. Materialize was used for the results table and the buttons on the page as well
+- **HTML:** I used a number of HTML tags. For user input specifically I used the input and select tags. Currently if the user logs in without github their data will be stored. However if they log in with 
+github their data will not be stored. This is something that needs to be fixed
+- **Javascript:** Javascript was used to do things like display username, add button functionality and of course manage communication between the client and server
 
-- **Tech Achievement 1**: This single-page app utilizes a combination of forms and tables so that a user can submit data, and the app will display both the data and the result of the derived field. The form includes a username, password, nickname, preferred foot, and position.
+### Technical Achievements
 
-### Design/Evaluation Achievements
+**Node.js server:** I implemented the github strategy using the passport middleware. Currently the user is able to login with thei github, however I was unable to store the github user id in the
+database. So currently the user can log in and add data but this data will not be persistent if the log in with github
 
-- **Student Interview 1**:
-
-Student Name: Lindberg Simpson  
-Problems:  No problems mentioned when using the site  
-Comments: The user didn't have any issues using the website. They did comment that it would be nice fot the entry forms to be aligned.  
-Changes to be made based on feedback: In future I will make sure that the forms are aligned. I usually like to focus on looks but this time I didnt have time to do that. So I will spend some time making it look nice
-
-- **Student Interview 2**:
-
-Student Name: Akul Argawal  
-Problems: No problems mentioned when using the site  
-Comments:  he user didn't have any issues using the website either. They did mention that it would be cool to display the football positions and display their peoples names.  
-Changes to be made based on feedback: The user seemed to be interested in having more of a graphical interface which I will try implement.
--- server.improved.js
+++ server.improved.js
@@ -3,156 +3,254 @@
 
 // init express
 const express = require("express");
-const favicon = require("serve-favicon");
+//const favicon = require("serve-favicon");
 const mongodb = require("mongodb")
 const env = require("dotenv/config");
+const GitHubStrategy = require('passport-github').Strategy;
 const app = express();
 const port = 3000;
-
-app.use(express.static("public"));
 
 //middleware
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const morgan = require("morgan")
+const helmet = require("helmet")
+const responseTime = require("response-time")
+
+
+app.use(express.static("public"));
+app.use(passport.initialize());
+app.use(morgan("combined"))
+app.use(responseTime())
+app.use(helmet())
+app.use(bodyParser.json())
 
 //Define mongo variables
 const MongoClient = mongodb.MongoClient;
-const uri = `mongodb+srv://hezi:${process.env.MONGO_PASS}@footballcluster.luqk2.mongodb.net/<dbname>?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, {
-  useNewUrlParser: true
-});
+const uri = `mongodb+srv://hezi:${process.env.MONGO_PASS}@footballcluster.luqk2.mongodb.net/A3-Database?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-let collection = null
+let currentUser = ""
+let isGitUser = null
+
+let playerCollection = null
+let userCollection = null
+let gitCollection = null
 
 
 client.connect(err => {
-  collection = client.db("test").collection("pleaseWork");
-  // perform actions on the collection object
-  console.log(collection);
+  gitCollection = client.db("A3-Database").collection("git_info");
+  playerCollection = client.db("A3-Database").collection("player_info");
+  userCollection = client.db("A3-Database").collection("user_info");
+  //console.log(collection)
+  //client.close()
 });
+
+
+// Serialize and deserialize functions needed for passport-github
+passport.serializeUser(function(user, done) { done(null, user) })
+passport.deserializeUser(function(user, done) { done(null, user) })
+
+passport.use(new GitHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: process.env.GITHUB_CALLBACK
+  },                      
+                                
+   async function(accessToken, refreshToken, profile, cb) {
+  
+    const githubUser = await gitCollection.find({githubUsername: profile.id}).toArray();
+    
+    gitCollection.insertOne({githubUsername: profile.username})
+  
+    currentUser = "profile.username"
+  
+    isGitUser = true
+  
+    cb(null, profile)
+    
+  
+  }));
+
+app.get("/auth/github", passport.authenticate("github"))
+
+app.get('/auth/github/callback', 
+  passport.authenticate('github', { failureRedirect: '/' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/public/teamSheet.html');
+  });
 
 //Handle requests from client
 app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/public/login.html");
+  if(currentUser != "") {
+    response.sendFile(__dirname + "/public/teamSheet.html");
+  }else {
+    response.sendFile(__dirname + "/public/login.html");
+  }
+  
 });
 
-app.post("/index.html", bodyParser.json(), (request, response) => {
-  console.log(request.body)
-});
+app.get('/public/teamSheet.html', (request, response) => {
+  if(currentUser == ""){
+    response.sendStatus(403)
+  }
+  else {
+    response.sendFile(__dirname + '/public/teamSheet.html')
+  }
+})
 
-app.post("/submit", bodyParser.json(), async (request, response) => {
-  console.log("body of server request:", request.body)
-  collection.insertOne(request.body)
-  .then( dbresponse => {
-    console.log(dbresponse)
+app.get('/isLoggedIn', (request, response) => {
+  if(currentUser == ""){
+    response.sendStatus(500)
+  }
+  else{
+    response.sendStatus(200)
+  }
+})
+
+app.get('/logout', (request, response) => {
+  if(isGitUser == true){
+    request.logout()
+  }
+  currentUser = ""
+  isGitUser = null
+  response.sendStatus(200)
+})
+
+app.get('/appdata',(request, response) => {
+  
+  let playerArray = []
+  
+  playerCollection.find({"account": currentUser}).forEach( doc => {
+    playerArray.push(doc)
+  })
+  .then( () => {
+    response.json(playerArray)
+  })
+  
+})
+
+app.get("/getCurrentUser", (request, response) => {
+  
+  const json = { 
+    user: currentUser
+  }
+  
+  response.json(json)
+  
+})
+
+app.post("/login", bodyParser.json(), async (request, response) => {
+  //console.log("body of request", request.body)
+  
+  if (currentUser != "") {
+    response.sendStatus(206)
+    return response.end()
+  }
+  else { 
+  
+    let userData = request.body
+    let user_name = userData.username
+    let user_pass = userData.password
+    
+    //console.log(user_name)
+    //console.log(user_pass)
+    //console.log(currentUser)
+    
+    isGitUser = false
+    
+    let user = await userCollection.find(user_name).toArray()
+    
+    //console.log(user)
+    
+    if(user.length == 0) {
+      userCollection.insertOne(userData)
+      currentUser = user_name
+      response.sendStatus(200)
+    }
+    else {
+      if(user[0].password == user_pass){
+        currentUser = user_name
+        response.sendStatus(200)
+      }
+      else {
+        currentUser = ""
+        response.sendStatus(500)
+      }
+    }
+  
+  }
+  
+  return response.end()
+  
+})
+
+app.post("/submit", bodyParser.json(), (request, response) => {
+  //console.log("body of server client:", request.body)
+  //console.log(currentUser)
+  //console.log(isGitUser)
+  
+  let addedPlayer = request.body
+  
+  addedPlayer.account = currentUser
+  addedPlayer.gitUser = isGitUser
+  
+  //console.log(addedPlayer)
+  
+  playerCollection.insertOne(addedPlayer)
+    .then( dbresponse => {
+      response.json(dbresponse.ops[0])
   })
 });
 
+app.post("/delete", bodyParser.json(), (request, response) => {
+  //console.log("body of server client:", request.body._id)
+  playerCollection.deleteOne({ _id:mongodb.ObjectID( request.body._id ) })
+    .then( () => {
+      let playerArray = []
+      
+      playerCollection.find({"account": currentUser}).forEach( doc => {
+        playerArray.push(doc)
+      })
+    .then( () => {
+        response.json(playerArray)
+      })
+  })
+});
+
+app.post("/edit", bodyParser.json(), (request, response) => {
+  
+  const json = { 
+    nick: request.body.nick,
+    foot: request.body.foot,
+    pos: request.body.pos,
+    dateAdded: request.body.dateAdded
+  }
+  
+  const id = request.body._id
+  
+  const newVal = { $set: json}
+  
+  playerCollection.updateOne( {_id:mongodb.ObjectID(request.body._id)}, newVal, (error, response) => {
+    if(error) throw error;
+    return
+  })
+  
+  let playerArray = []
+  
+  playerCollection.find({"account": currentUser}).forEach( doc => {
+    playerArray.push(doc)
+  })
+  .then( () => {
+    response.json(playerArray)
+  })
+
+})
 
 // listen for requests :)
-const listener = app.listen(port, function() {
+const listener = app.listen(process.env.PORT, () => {
   console.log('Your app is listening on port ' + listener.address().port);
   console.log(process.env.DEMON_DAYS);
 });
 
-
-/*
-
-app.post("/index.html", bodyParser.json(), (request, response) => {
-  consloe.log(request.body)
-});
-
-
-const mime = require( 'mime' ),
-      dir  = 'public/',
-
-app.use( function(request, response, next) {
-  console.log(request.url)
-  next()
-});
-
-app.use(express.static("public"));
-
-
-
-
-const appdata = [
-  { id: 0, username: "Hezi", Password: "hello", Foot: "left", Position: "def", dateJoined: "2020-9-21" }
-];
-
-
-
-let playerPage = null;
-fs.readFile("public/players.html", function(err, content) {
-  playerPage = content;
-});
-
-const server = http.createServer( function( request,response ) {
-  if( request.method === 'GET' ) {
-    handleGet( request, response )
-  }else if( request.method === 'POST' ){
-    handlePost( request, response )
-  }
-})
-
-const handleGet = function( request, response ) {
-  const filename = dir + request.url.slice( 1 )
-  if( request.url === '/' ) {
-    sendFile( response, 'public/index.html' )
-  }
-  else if( request.url === "/players") {
-    sendFile( response, "public/players.html")
-  }
-  else if( request.url === "/game") {
-    sendFile( response, "public/game.html");
-  }
-  else if(request.url === "/style.css") {
-    sendFile( response, "public/css/style.css");
-  }
-  else{
-    sendFile( response, filename )
-  }
-}
-
-const handlePost = function( request, response ) {
-  let dataString = ''
-
-  request.on( 'data', function( data ) {
-      dataString += data
-  })
-
-  request.on( 'end', function() {
-    console.log( JSON.parse( dataString ) )
-
-    // ... do something with the data here!!!
-
-    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-    response.end()
-  })
-}
-
-const sendFile = function( response, filename ) {
-   const type = mime.getType( filename )
-
-   fs.readFile( filename, function( err, content ) {
-
-     // if the error = null, then we've loaded the file successfully
-     if( err === null ) {
-
-       // status code: https://httpstatuses.com
-       response.writeHeader( 200, { 'Content-Type': type })
-       response.end( content )
-
-     }else{
-
-       // file not found, error code 404
-       response.writeHeader( 404 )
-       response.end( '404 Error: File Not Found' )
-
-     }
-   })
-}
-
-server.listen( process.env.PORT || port )
-
-*/
